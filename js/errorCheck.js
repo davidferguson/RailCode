a2005f9a1f9df83a726532f400021661ece67630
davidferguson <fergusondavid6@gmail.com>
2015-07-29 14:06:41 +0100

    removed h4 and firstchild from info div

--- js/errorCheck.js
+++ js/errorCheck.js
@@ -9,7 +9,7 @@ function pageLoad(gameMode, stage)
 		mode = 'learn';
 		currentStage = stage;
 		setup(stages[currentStage].step[currentStep].objectToUse, stages[currentStage].step[currentStep].startLine, stages[currentStage].step[currentStep].startStation);
-		document.getElementById('trainInfoPanel').firstChild.innerHTML = '<h4>' + stages[currentStage].step[currentStep].instruction + '</h4>';
+		document.getElementById('trainInfoPanel').innerHTML = stages[currentStage].step[currentStep].instruction;
 	}
 	else
 	{
@@ -21,6 +21,7 @@ function pageLoad(gameMode, stage)
 
 function checkResult(stepSuccess, tubeStations, tubeEndLine, userCode, error, requiredStation)
 {
+	console.log(tubeStations);
 	//this function is run when the user has typed their code, pressed 'Run Code', and the code has been compiled and run
 	
 	var continueOn = true;
@@ -29,20 +30,37 @@ function checkResult(stepSuccess, tubeStations, tubeEndLine, userCode, error, re
 	{
 		//code to be run to check the users attempt during the LEARN mode
 		
-		if (stages[currentStage].step[currentStep].stepSuccess != stepSuccess) {continueOn = false;}
-		if (stages[currentStage].step[currentStep].endLine != tubeEndLine) {continueOn = false;}
+		if (stages[currentStage].step[currentStep].stepSuccess != stepSuccess)
+		{
+			continueOn = false;
+		}
+		if (stages[currentStage].step[currentStep].endLine != tubeEndLine)
+		{
+			console.log(2);
+			continueOn = false;
+		}
 		
 		if (stages[currentStage].step[currentStep].stations.length != 1)
 		{
 			for (var i = 0; i < stages[currentStage].step[currentStep].stations.length; i++)
 			{
 				//loop through each string in the mustInclude array
-				if (stages[currentStage].step[currentStep].stations[i] != tubeStations[i]) {continueOn = false;}
+				if (stages[currentStage].step[currentStep].stations[i] != tubeStations[i])
+				{
+					console.log(3);
+					continueOn = false;
+				}
 			}
 		}
 		else
 		{
-			if (stages[currentStage].step[currentStep].stations[stages[currentStage].step[currentStep].stations.length -1] != tubeStations[tubeStations.length -1]) {continueOn = false;}
+			if (stages[currentStage].step[currentStep].stations[stages[currentStage].step[currentStep].stations.length -1] != tubeStations[tubeStations.length -1])
+			{
+				console.log(stages[currentStage].step[currentStep].stations[stages[currentStage].step[currentStep].stations.length -1]);
+				console.log(tubeStations[tubeStations.length -1]);
+				console.log(4);
+				continueOn = false;
+			}
 		}
 		
 		for (var i = 0; i < stages[currentStage].step[currentStep].mustInclude.length; i++)
@@ -50,6 +68,7 @@ function checkResult(stepSuccess, tubeStations, tubeEndLine, userCode, error, re
 			//loop through each string in the mustInclude array
 			if (userCode.indexOf(stages[currentStage].step[currentStep].mustInclude[i]) == -1)
 			{
+				console.log(5);
 				continueOn = false;
 			}
 		}
@@ -58,7 +77,10 @@ function checkResult(stepSuccess, tubeStations, tubeEndLine, userCode, error, re
 	{
 		//code to be run to check the user's attempt during the PLAY mode
 		if (!stepSuccess) {continueOn = false;}
-		if (tubeStations[tubeStations.length -1] != requiredStation) {continueOn = false;}
+		if (tubeStations[tubeStations.length -1] != requiredStation)
+		{
+			continueOn = false;
+		}
 	}
 	if (continueOn)
 	{
@@ -78,7 +100,7 @@ function displayCorrect()
 	//it presents a nice message and asks them to continue
 	
 	document.getElementById('trainInfoPanel').style.backgroundColor = '#BCF5A9';
-	document.getElementById('trainInfoPanel').firstChild.innerHTML = '<h4>Well Done! You got that spot on.</h4>';
+	document.getElementById('trainInfoPanel').innerHTML = 'Well Done! You got that spot on.';
 	document.getElementById('codeButtons').innerHTML = '<button style="width: 100%; height: 100%; background-color: #BCF5A9;" onclick="nextStep();">Continue</button>'
 }
 
@@ -87,14 +109,14 @@ function displayIncorrect(error)
 	//this function is run if the user's code was incorrect for the learn mode
 	//it presents an error message and asks them to retry
 	
-	if (!stages[currentStage].step[currentStep].hasOwnProperty(customError) || error == '')
+	if (!stages[currentStage].step[currentStep].hasOwnProperty("customError") || error == '')
 	{
-		document.getElementById('trainInfoPanel').firstChild.innerHTML = "<h4>Oh dear! That didn't quite work.</h4>";
+		document.getElementById('trainInfoPanel').innerHTML = "Oh dear! That didn't quite work.";
 	}
 	else
 	{
 		//there is a custom error message that needs to be sent to the user in addition to the standard error message
-		document.getElementById('trainInfoPanel').firstChild.innerHTML = "<h4>Oh dear! That didn't quite work. " + error + ' ' + stages[currentStage].step[currentStep].customError + "</h4>";
+		document.getElementById('trainInfoPanel').innerHTML = "Oh dear! That didn't quite work. " + error + ' ' + stages[currentStage].step[currentStep].customError;
 	}
 	document.getElementById('trainInfoPanel').style.backgroundColor = '#F5A9A9';
 	document.getElementById('codeButtons').innerHTML = '<button style="width: 100%; height: 100%; background-color: #F5A9A9;" onclick="retryStep();">Retry</button>'
@@ -127,7 +149,7 @@ function nextStep()
 			currentStep++;
 		}
 		document.getElementById('trainInfoPanel').style.backgroundColor = '#F2F5A9';
-		document.getElementById('trainInfoPanel').firstChild.innerHTML = '<h4>' + stages[currentStage].step[currentStep].instruction + '</h4>';
+		document.getElementById('trainInfoPanel').innerHTML = stages[currentStage].step[currentStep].instruction;
 		document.getElementById('codeButtons').innerHTML = '<button style="width: 100%; height: 100%;" onclick="compileAndRun();">Run</button>';
 		
 		//Now send the object, line and station to the function that handles all the map loading
