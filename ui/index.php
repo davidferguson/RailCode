c3f764dfbfa0798278e9bf1bdfef9b8c3f7245b3
davidferguson <fergusondavid6@gmail.com>
2015-07-30 13:20:10 +0100

    updated to always redirect to home.php

--- ui/index.php
+++ ui/index.php
@@ -1,11 +1,3 @@
 <?php
-	session_start();
-	if( ! isset($_SESSION["email"]) )
-	{
-		header("Location: login.php");
-	}
-	else
-	{
-		header("Location: home.php");
-	}
+	header("Location: home.php");
 ?>
