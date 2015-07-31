d6a451dafd22547c09a37d39f47e6f4e0af4014b
Andrew Ferguson <andrew@andrew-debian-laptop.(none)>
2015-07-31 21:11:37 +0100

    Added complete message for first stage

--- js/stageCompleteMsg.js
+++ js/stageCompleteMsg.js
@@ -1,6 +1,6 @@
 var stageCompleteMsg = [
-	'Congratulations.',
-	'Congratulations.',
+	'Congratulations! You have just completed stage 1 of 9. Your progress has now been saved, so you can safely log out if you want to take a break. Or, if you want to continue, the next stage in the Learn section has been unlocked, so you can head right on...',
+	'Congratulations',
 	'Congratulations.',
 	'Congratulations.',
 	'Congratulations.',
